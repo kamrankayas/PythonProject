57f846e3768d70bbd1025b66196610babd7a0f7f
Kamran Ahmed Kayas HE19851 <kamran.he19851@student.rcl.ac.uk>
2023-12-20 14:37:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#Example of Basic Ductionary: 1. Name the Dictionary
+#2. Key: String, 3.Value: Any Datatype, 4. Paring with Colon 5. Curly Bracket.
+StudentID={
+    1 : "Kayas",
+    2 : "Saju",
+    3 : "Kaif",
+}
+print(StudentID[3])
